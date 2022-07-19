9d7bd322576db10deb1efb1465d263d36420ac2f
whayter <hwinston@student.42.fr>
2022-07-19 12:35:07 +0200

    level1 done

--- level1/source.c
+++ level1/source.c
@@ -3,14 +3,14 @@
 
 int run()
 {
-    fwrite("Good... Wait what?\n", 19, 1, stdout);
-	return (system("/bin/sh"));
+    fwrite("Good... Wait what?\n", 19, 1, stdout);      //  run +  6 -  run + 41
+	return (system("/bin/sh"));                         //  run + 46 -  run + 59
 }
 
 int main(void)
 {
-    char buf[76];
+    char buf[64];                                       // main +  9 (0x50 - 0x10 = 64)
 
-    gets(buf);
-    return (0);
+    gets(buf);                                          // main + 13 - main + 16
+    return (0);                                         // main + 21 - main + 22
 }
