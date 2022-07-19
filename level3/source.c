32715e9ec83897b41278c9cf6af68773622ff5c4
whayter <hwinston@student.42.fr>
2022-07-19 14:08:54 +0200

    level3 done

--- level3/source.c
+++ level3/source.c
@@ -1,24 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int m;
+int m;                                              // 0x0804988c
 
 void v(void)
 {
-    char buf[520];
+    char buf[512];                                  // from -0x208(%ebp) to 0x8(%esp)
 
-    fgets(buf, 512, stdin);
-    printf(buf);
-    if (m == 64)
+    fgets(buf, 512, stdin);                         // v +   9 - v +  35
+    printf(buf);                                    // v +  40 - v +  49
+    if (m == 64)                                    // v +  54 - v +  62
     {
-        fwrite("Wait what?!\n", 12, 1, stdout);
-        system("/bin/sh");
+        fwrite("Wait what?!\n", 12, 1, stdout);     // v +  64 - v +  99
+        system("/bin/sh");                          // v + 104 - v + 111
     }
-    return;
+    return;                                         // v + 116 - v + 117
 }
 
 int main(void)
 {
-    v();
-    return (0);
+    v();                                            // main +  6
+    return (0);                                     // main + 11 - main + 12
 }
