11481fcfb27992b91c232d86f3b3052144d02aef
whayter <hwinston@student.42.fr>
2022-07-19 12:14:32 +0200

    level0 done

--- level0/source.c
+++ level0/source.c
@@ -12,18 +12,18 @@ int main(int ac, char **av)
 	gid_t	gid;
 	uid_t	uid;
 	
-	n = atoi(av[1]);
-	if (n != 423)
-		fwrite("No !\n", 5, 1, stderr);
-	else
+	n = atoi(av[1]);							// main +   9 - main +  20
+	if (n != 423)								// main +  25 - main +  30
+		fwrite("No !\n", 5, 1, stderr);			// main + 152 - main + 187
+	else										// main + 150
 	{
-		cmds[0] = strdup("/bin/sh");
-		cmds[1] = NULL;
-		gid = getegid();
-		uid = geteuid();
-		setresgid(gid, gid, gid);
-		setresuid(uid, uid, uid);
-		execv("/bin/sh", cmds);
+		cmds[0] = strdup("/bin/sh");			// main +  32 - main +  44
+		cmds[1] = NULL;							// main +  48  
+		gid = getegid();						// main +  56 - main +  61
+		uid = geteuid();						// main +  65 - main +  70
+		setresgid(gid, gid, gid);				// main +  74 - main +  97
+		setresuid(uid, uid, uid);				// main + 102 - main + 125
+		execv("/bin/sh", cmds);					// main + 130 - main + 145
 	}
-    return (0);
+    return (0);									// main + 192 - main + 198
 }
--- level0/walkthrough.md
+++ level0/walkthrough.md
@@ -1,11 +1,12 @@
-The goal of this project is to get the content of the .pass file in the current directory of the top level.
+The goal of this project is to get the content, for each level, of the .pass file that is in the current directory of the upper level. We are currently at level0, so we want to get the content of the file /home/user/level1/.pass:
+
 ```
 level0@RainFall:~$ cat /home/user/level1/.pass
 cat: /home/user/level1/.pass: Permission denied
 ```
 Of course, that whould be too easy.
 
-We have an executable file named level0:
+There is an executable file named level0:
 ```
 level0@RainFall:~$ ./level0 
 Segmentation fault (core dumped)
@@ -29,4 +30,4 @@ $ cat /home/user/level1/.pass
 1fe8a524fa4bec01ca4ea2a869af2a02260d4a7d5fe7e7c24d8617e6dca12d3a
 ```
 
-We can go to the next level with the command `su level1` and fill in the flag found as a password.
+We can now reach the next level with the command `su level1` and fill in the flag found as a password.
